de6d933593793ab6a0834bc8c34ec7294b376341
sachin-source <sachinmgvt@gmail.com>
2024-06-03 19:28:29 +0530

    added home page

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const homePage = () => {
+  return (
+    <div className="text-8xl text-red-400" >homePage
+    <Button >ShadCN</Button>
+    </div>
+  )
+}
+
+export default homePage
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const homePage = () => {
-  return (
-    <div className="text-8xl text-red-400" >homePage</div>
-  )
-}
-
-export default homePage
