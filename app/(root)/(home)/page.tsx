881a2e0827300df0ea8dcafa935bf709c765fcfc
sachin-source <sachinmgvt@gmail.com>
2024-06-03 19:45:49 +0530

    basic page with navbar init

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,10 @@
+import Home from '@/components/Home/Home'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const homePage = () => {
   return (
-    <div className="text-8xl text-red-400" >homePage
-    <Button >ShadCN</Button>
-    </div>
+    <Home />
   )
 }
 
