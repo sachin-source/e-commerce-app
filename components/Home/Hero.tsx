de7ddbcac72ff24bec1fe8ec354ebe3097c2063e
sachin-source <sachinmgvt@gmail.com>
2024-06-06 19:56:58 +0530

    started with Hero component

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='w-full h-[calc(100vh-12vh)]' >Hero</div>
+    <div className='w-full h-[calc(100vh-12vh)] flex justify-center flex-col' >
+        <div className="w-4/5 mx-auto grid items-center grid-cols-1 "></div>
+    </div>
   )
 }
 
