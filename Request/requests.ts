f75874d94214d6214a210fd0dc064a1c7dfb7b8b
sachin-source <sachinmgvt@gmail.com>
2024-06-07 18:52:06 +0530

    created new type file and started with AllProducts page

--- Request/requests.ts
+++ Request/requests.ts
@@ -1,4 +1,9 @@
 export async function getAllCategory() {
     const categoryRes = await fetch("https://fakestoreapi.com/products/categories");
-    return categoryRes.json()
+    return categoryRes.json();
+}
+
+export async function getAllProduct() {
+    const productRes = await fetch('https://fakestoreapi.com/products');
+    return productRes.json();
 }
