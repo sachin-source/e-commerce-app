859b749a71ad222232502c79821ba5308ce5ad47
sachin-source <sachinmgvt@gmail.com>
2024-06-07 09:59:55 +0530

    Started with Category section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Hero from './Hero'
+import Category from './Category'
 
 const Home = () => {
   return (
-    <div><Hero /></div>
+    <div>
+      <Hero />
+      <Category />
+    </div>
   )
 }
 
