df76e0edb63eefd6e4100e459c61b40668023085
sachin-source <sachinmgvt@gmail.com>
2024-06-07 19:25:26 +0530

    All products component in progress and start with product component

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero'
 import Category from './Category'
+import AllProduct from './AllProduct'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Category />
+      <AllProduct />
     </div>
   )
 }
