c37c523d8562ad9af069f5589e122e1503afa407
sachin-source <sachinmgvt@gmail.com>
2024-06-08 19:21:07 +0530

    Add to cart button init

---
+++ app/(root)/product/product-details/add-cart.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <Button className='mt-6'>Add to Cart</Button>
+  )
+}
+
+export default AddToCart
