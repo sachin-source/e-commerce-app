712c12cd54fd7d2dc1c7df1ec7553348c11ee72c
sachin-source <sachinmgvt@gmail.com>
2024-06-10 19:34:42 +0530

    Cart store complete

--- store/store.ts
+++ store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import cartReducer from "./cartSlice"
+
 const store = configureStore({
     reducer : {
-        // cart : cartReducer
+        cart : cartReducer
     }
 })
 
