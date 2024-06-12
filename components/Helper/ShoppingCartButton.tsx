234d62422b838e7bda35bc0780851580f391e3f2
sachin-source <sachinmgvt@gmail.com>
2024-06-12 18:21:32 +0530

    Add to cart functionality done

--- components/Helper/ShoppingCartButton.tsx
+++ components/Helper/ShoppingCartButton.tsx
@@ -10,7 +10,7 @@ const ShoppingCartButton = () => {
 
   return (
     <div className='relative' >
-        <span className='absolute -top-3 -right-2 w-6 h-6 bg-red-500 text-center flex items-center justify-center flex-col text-xs text-white rounded-full' >6</span>
+        <span className='absolute -top-3 -right-2 w-6 h-6 bg-red-500 text-center flex items-center justify-center flex-col text-xs text-white rounded-full' >{totalQuantity}</span>
         <ShoppingBagIcon cursor={"pointer"} size={26} />
     </div>
   )
