9e5287e4da0a503e5ff13b8c7101e32849050a9b
sachin-source <sachinmgvt@gmail.com>
2024-06-12 18:35:52 +0530

    Started with cart sidebar component

---
+++ components/Helper/CartSidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CartSidebar = ({}) => {
+  return (
+    <div>CartSidebar</div>
+  )
+}
+
+export default CartSidebar
--- components/Helper/ShoppingCartButton.tsx
+++ components/Helper/ShoppingCartButton.tsx
@@ -3,16 +3,25 @@ import { RootState } from '@/store/store';
 import { ShoppingBagIcon } from 'lucide-react'
 import React from 'react'
 import { useSelector } from 'react-redux';
+import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet';
+import CartSidebar from './CartSidebar';
 
 const ShoppingCartButton = () => {
   const items = useSelector((state:RootState) => state.cart.items);
   const totalQuantity = items.reduce((total, item) => total + item.quantity, 0);
 
   return (
-    <div className='relative' >
+    <Sheet>
+      <SheetTrigger>
+      <div className='relative' >
         <span className='absolute -top-3 -right-2 w-6 h-6 bg-red-500 text-center flex items-center justify-center flex-col text-xs text-white rounded-full' >{totalQuantity}</span>
         <ShoppingBagIcon cursor={"pointer"} size={26} />
     </div>
+      </SheetTrigger>
+      <SheetContent className='overflow-auto h-full' >
+        <CartSidebar />
+      </SheetContent>
+    </Sheet>
   )
 }
 
