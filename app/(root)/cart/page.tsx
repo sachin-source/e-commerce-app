2e44a0fe64a155f8fdafe1bac45cb64df35c740c
sachin-source <sachinmgvt@gmail.com>
2024-06-14 09:14:47 +0530

    Cart page complete

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { Button } from '@/components/ui/button';
+import { CartItem, addItem, removeItem } from '@/store/cartSlice';
 import { RootState } from '@/store/store';
+import { useUser } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Cart = () => {
     // Get our cart items
@@ -15,6 +17,13 @@ const Cart = () => {
     const totalPrice = items.reduce((total, item) => total + (item.price + item.quantity), 0);
     const vat = (+totalPrice * 0.15).toFixed(2);
     const totalPreviewWithVat = (+totalPrice + +vat).toFixed(2);
+
+    // Get authenticate user
+    const { user } = useUser()
+    const dispatch = useDispatch()
+    
+    const addItemHandler = (item:CartItem) => dispatch(addItem(item));
+    const removeItemHandler = (id:number) => dispatch(removeItem({id}));
 
     return (
         <div className='mt-8 min-h-[60vh]' >
@@ -47,8 +56,8 @@ const Cart = () => {
                                             <h1 className='md:text-2xl text-lg font-bold text-blue-950' >${item.price}</h1>
                                             <h1 className='md:text-lg text-sm font-semibold' >Quantity : {item.quantity}</h1>
                                             <div className="flex items-center mt-4 space-x-2">
-                                                <Button>Add More</Button>
-                                                <Button variant={'destructive'} >Remove</Button>
+                                                <Button onClick={() => addItemHandler(item)} >Add More</Button>
+                                                <Button variant={'destructive'} onClick={() => removeItemHandler(item.id)} >Remove</Button>
                                             </div>
                                         </div>
                                     </div>
@@ -59,7 +68,34 @@ const Cart = () => {
                     {/* Cart Summary */}
                     <div className="xl:col-span-2">
                         <div className="bg-indigo-950 sticky top-[25vh] p-6 rounded-lg">
-                            <div className="text-center mt-8 mb-8 text-white text-zxl font-semibold">Summary</div>
+                            <h1 className="text-center mt-8 mb-8 text-white text-3xl font-semibold">Summary</h1>
+                            <div className="w-full h-[1.2px] bg-white bg-opacity-20"></div>
+                            <div className="flex mt-4 text-xl uppercase font-semibold text-white items-center justify-between">
+                                    <span>Subtotal</span>
+                                    <span>${totalPrice}</span>
+                                </div>
+                                <div className="flex mt-10 mb-10 text-xl uppercase font-semibold text-white items-center justify-between">
+                                    <span>VAT</span>
+                                    <span>${vat}</span>
+                                </div>
+                                <div className="flex mb-6 text-xl uppercase font-semibold text-white items-center justify-between">
+                                    <span>Shipping</span>
+                                    <span>Free</span>
+                                </div>
+                                <div className="w-full h-[1.2px] bg-white bg-opacity-20"></div>
+                                <div className="flex mt-6 mb-6 text-xl uppercase font-semibold text-white items-center justify-between">
+                                    <span>Total</span>
+                                    <span>${totalPreviewWithVat}</span>
+                                </div>
+                                {!user && (
+                                    <Link href="/sign-in" >
+                                        <Button className='bg-orange-500 w-full' >Sign In to Checkout</Button>
+                                    </Link>
+                                )}
+
+                                {user && (
+                                    <Button className='bg-orange-500 w-full' >Paypal</Button>
+                                )}
                         </div>
                     </div>
                 </div>
