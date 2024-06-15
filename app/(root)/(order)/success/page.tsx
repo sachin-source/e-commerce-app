41dc1c37a7f8c78b2d1293a90bd4ecce1c7a22bd
sachin-source <sachinmgvt@gmail.com>
2024-06-15 13:28:07 +0530

    success page complete

--- app/(root)/(order)/success/page.tsx
+++ app/(root)/(order)/success/page.tsx
@@ -1,8 +1,17 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='w-full h-[60vh] flex items-center justify-center flex-col' >
+        <Image src="/images/order.svg" alt='success' width={300} height={300} />
+        <h1 className='mb-8 text-3xl mt-2 font-bold uppercase text-green-600' >Order Successful</h1>
+        <Link href="/" >
+        <Button>Go To Home</Button>
+        </Link>
+    </div>
   )
 }
 
