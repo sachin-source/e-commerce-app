24d60f5518df91e2e02a22492eb50165d79aa17b
sachin-source <sachinmgvt@gmail.com>
2024-06-15 13:00:50 +0530

    paypal integration complete

--- components/Helper/PayPalButton.tsx
+++ components/Helper/PayPalButton.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+// @ts-nocheck
+import React from 'react';
+import { FUNDING, PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
 
-const PayPalButton = () => {
+interface PayPalButtonProps {
+    amount: string;
+    onSuccess: (details: any) => void;
+}
+
+const PayPalButton = ( {amount, onSuccess}:PayPalButtonProps ) => {
   return (
-    <div>PayPalButton</div>
+   <PayPalScriptProvider options={{
+    clientId : process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!,
+    currency : 'USD'
+   }} >
+    <PayPalButtons fundingSource={FUNDING.PAYPAL} createOrder={(data, actions) => {
+        return actions.order.create({
+            purchase_units:[
+                {
+                    amount: {
+                        value:amount
+                    }
+                }
+            ]
+        })
+    }}
+    onApprove={(data, actions) => {
+        return actions.order.capture().then((details) => {
+            onSuccess(details)
+        })
+    }} />
+   </PayPalScriptProvider>
   )
 }
 
